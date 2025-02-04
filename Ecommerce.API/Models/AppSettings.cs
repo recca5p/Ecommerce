c8c0b26b6f9f60b926ca54a0aa6d39ee4084d576
Phat Vo <recca5p@gmail.com>
2025-02-04 08:59:31 +0700

    add db

---
+++ Ecommerce.API/Models/AppSettings.cs
@@ -0,0 +1,6 @@
+namespace Ecommerce.API.Models;
+
+public class AppSettings
+{
+    public string DatabaseConnectionString {get; set;}
+}
--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -1,12 +1,51 @@
-var builder = WebApplication.CreateBuilder(args);
+using Domain.RepositoriyInterfaces;
+using Ecommerce.API.Middlewares;
+using Ecommerce.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Persistence.Repositories;
+using Services;
+using Services.Abstraction;
+using Services.Profiles;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+    .Build();
+
+String envInUsed = configuration.GetSection("ASPNETCORE_ENVIRONMENT").Value 
+                   ?? throw new InvalidOperationException("Can not get ASPNETCORE_ENVIRONMENT value");
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    EnvironmentName = envInUsed
+});
 
 // Add services to the container.
+
+IConfigurationSection appConfigSection = builder.Configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSettings>(appConfigSection);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string connectionString = builder.Configuration.GetSection("AppSettings:DatabaseConnectionString").Value 
+                          ?? throw new InvalidOperationException("Can not get DatabaseConnectionString");
+
+builder.Services.AddScoped<IRepositoryDbContext>(x =>
+    new RepositoryDbContext(
+        new DbContextOptionsBuilder<RepositoryDbContext>()
+            .UseNpgsql(connectionString)
+            .Options));
+
+builder.Services.AddTransient<ExceptionHandlingMiddleware>();
+
+builder.Services.AddAutoMapper(typeof(ModelProfile));
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
+    
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +58,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.MapControllers();
 
